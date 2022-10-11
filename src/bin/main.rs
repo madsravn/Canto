2a1d0a245afa5dea90d8daa8c23908f2a9d5b6b4
Mads Ravn <mads.ravn@lego.com>
2022-10-11 16:07:45 +0200

    comparisons

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,20 +1,34 @@
+use canto::overlap;
 use std::env;
 use std::fs;
 
 pub fn main() {
     let args: Vec<_> = env::args().collect();
     if args.len() > 1 {
-        let input: Vec<String> = fs::read_to_string(&args[1]).expect("Should be able to open file").split("\n").filter(|s| s.len() > 0).map(|s| s.to_string()).collect();
+        let input: Vec<String> = fs::read_to_string(&args[1])
+            .expect("Should be able to open file")
+            .split("\n")
+            .filter(|s| s.len() > 0)
+            .map(|s| s.to_string())
+            .collect();
+        let mut input_one = input.clone();
+        input_one.truncate(input_one.len() - 1);
+        for (i, sound_one) in input_one.iter().enumerate() {
+            for sound_two in input.iter().skip(i + 1) {
+                let sound_one_o = overlap::open_sound(sound_one);
+                let sound_two_o = overlap::open_sound(sound_two);
+                let result = overlap::find_overlap(&sound_one_o, &sound_two_o);
+                if result.len() > 0 {
+                    println!("Equalities between {} and {}", sound_one, sound_two);
+                    println!("{:?}", result);
+                }
+            }
+        }
 
-
-
-    // TODO: Read all files in
-    // Compare to each other and find similarities
-    // Print similarities and also create files holding the similarities so we can verify.
-    //
-    // 8 KHz and 16 KHz. Can we compare these at all? 
-
+        // TODO: Read all files in
+        // Compare to each other and find similarities
+        // Print similarities and also create files holding the similarities so we can verify.
+        //
+        // 8 KHz and 16 KHz. Can we compare these at all?
     }
-
-
 }
--- src/overlap.rs
+++ src/overlap.rs
@@ -2,26 +2,88 @@ use hound;
 use std::collections::HashMap;
 
 pub struct Sound {
-    sound: Vec<i16>,
+    samples: Vec<i16>,
     index: HashMap<i16, Vec<usize>>,
     sample_rate: u32,
 }
 
+#[derive(Debug)]
+pub struct Position {
+    pub start_one: usize,
+    pub start_two: usize,
+    pub length: usize,
+}
 
 pub fn open_sound(filename: &str) -> Sound {
     let reader = hound::WavReader::open(filename).expect("Should be able to open file");
     let sample_rate = reader.spec().sample_rate;
-    let samples: Vec<i16> = reader.into_samples().filter(|s| s.is_ok()).map(|s| s.unwrap()).collect();
+    let samples: Vec<i16> = reader
+        .into_samples()
+        .filter(|s| s.is_ok())
+        .map(|s| s.unwrap())
+        .collect();
     let mut index = HashMap::new();
     for (i, s) in samples.iter().enumerate() {
         index.entry(*s).or_insert(Vec::new()).push(i);
     }
 
+    Sound {
+        samples,
+        index,
+        sample_rate,
+    }
+}
 
+/// Iterate over needle_sound and see how many places in master_sound it is present.
+pub fn find_overlap(master_sound: &Sound, needle_sound: &Sound) -> Vec<Position> {
+    let mut vec = Vec::new();
 
-    Sound { sound: samples, index, sample_rate }
+    for (i, sample) in needle_sound.samples.iter().enumerate() {
+        if let Some(positions) = master_sound.index.get(sample) {
+            for position in positions.iter() {
+                // Look here if we need to continue
+                if check_if_continue(*position, i, &vec) {
+                    continue
+                }
+                let length = find_similar_length(master_sound, needle_sound, *position, i);
+                let pos = Position {
+                    start_one: i,
+                    start_two: *position,
+                    length,
+                };
 
+                if pos.length > 20 {
+                    println!("Adding {:?}", pos);
+                    vec.push(pos);
+                }
+            }
+        }
+    }
+    vec
 }
 
 
+fn check_if_continue(start_one: usize, start_two: usize, positions: &Vec<Position>) -> bool {
 
+
+
+    true
+}
+
+fn find_similar_length(
+    sound_one: &Sound,
+    sound_two: &Sound,
+    sound_one_start_pos: usize,
+    sound_two_start_pos: usize,
+) -> usize {
+    let mut length: usize = 0;
+    while &sound_one.samples.get(sound_one_start_pos + length)
+        == &sound_two.samples.get(sound_two_start_pos + length)
+    {
+        if &sound_one.samples.get(sound_one_start_pos + length) == &None {
+            break;
+        }
+        length = length + 1;
+    }
+    length
+}
